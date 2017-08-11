fd3cbe39bafec3a8e04994c3a854e941310707d2
Rosa <soosg0822@naver.com>
2017-08-11 17:38:24 +0900

    doneparsingandwilldotable

--- Parsing.py
+++ Parsing.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 with open("patent_050104.xml", "r", encoding = "utf8") as patent_xml:
     xml = patent_xml.read()
 
-soup = BeautifulSoup(xml, "html.parser")
+soup = BeautifulSoup(xml, "lxml")
 # print(soup)
 
 
@@ -26,7 +26,7 @@ import pprint
 import xmltodict
 doc = xmltodict.parse(xml)
 
-#pprint.pprint(doc)
+# pprint.pprint(doc)
 
 
 
@@ -34,7 +34,7 @@ doc = xmltodict.parse(xml)
 #변수 설정
 
 
-patent_country= doc['us-patent-grant']['@country']
+upg_patent_country= doc['us-patent-grant']['@country']
 upg_date_produced = doc['us-patent-grant']['@date-produced']
 upg_date_publ = doc['us-patent-grant']['@date-publ']
 upg_dtd= doc['us-patent-grant']['@dtd-version']
@@ -89,11 +89,49 @@ ubdg_claims_text = doc['us-patent-grant']['claims']['claim']['claim-text']
 
 
 #citaion  변수설쩡
+#patcit만.
 
 patcit=[]
 for i in range(len(doc['us-patent-grant']['us-bibliographic-data-grant']['references-cited']['citation'])):
     patcit.append((doc['us-patent-grant']['us-bibliographic-data-grant']['references-cited']['citation'])[i])
-print(patcit[0])
+# print(patcit[1])
+
+patcit_num = []
+patcit_country = []
+patcit_doc_number = []
+patcit_kind = []
+patcit_name = []
+patcit_date = []
+
+for j in patcit:
+    patcit_number = j['patcit']['@num']
+    patcit_doc_country = j['patcit']['document-id']['country']
+    patcit_doc_docnumber = j['patcit']['document-id']['doc-number']
+    patcit_doc_kind = j['patcit']['document-id']['kind']
+    patcit_doc_name = j['patcit']['document-id']['name']
+    patcit_doc_date = j['patcit']['document-id']['date']
+    patcit_num.append(patcit_number)
+    patcit_country.append(patcit_doc_country)
+    patcit_doc_number.append(patcit_doc_docnumber)
+    patcit_kind.append(patcit_doc_kind)
+    patcit_name.append(patcit_doc_name)
+    patcit_date.append(patcit_doc_date)
+
+#patcit 다 리스트 처리
+
+# print(patcit_num)
+# print(patcit_country)
+# print(patcit_kind)
+# print(patcit_name)
+# print(patcit_date)
+# print(patcit_doc_number)
+
+# for i in range(8):
+#     for j in patcit:
+#         pacit_num = patcit
+
+
+# print()
 
 # for k in patcit:
 #     for p in patcit[k]:
@@ -104,6 +142,19 @@ print(patcit[0])
 classification_national = []
 for i in range(len(doc['us-patent-grant']['us-bibliographic-data-grant']['field-of-search']['classification-national'])):
     classification_national.append((doc['us-patent-grant']['us-bibliographic-data-grant']['field-of-search']['classification-national'])[i])
+# print(classification_national[0])
+
+classification_country = []
+classification_main = []
+
+for i in classification_national:
+    classificiation_c = i['country']
+    classificiation_m = i['main-classification']
+    classification_country.append(classificiation_c)
+    classification_main.append(classificiation_m)
+# print(classification_country)
+# print(classification_main)
+
 
 # print(classification_national[0])
 
@@ -111,13 +162,49 @@ for i in range(len(doc['us-patent-grant']['us-bibliographic-data-grant']['field-
 figure = []
 for i in range(len(doc['us-patent-grant']['drawings']['figure'])):
     figure.append((doc['us-patent-grant']['drawings']['figure'])[i])
+# print(figure)
 
-# print(figure[0])
+figure_id = []
+figure_num = []
+figure_img_id = []
+figure_img_he =[]
+figure_img_wi = []
+figure_img_file = []
+figure_img_alt = []
+figure_img_content =[]
+figure_img_format = []
 
+for i in figure:
+    figure_id.append(i['@id'])
+    figure_num.append(i['@num'])
+    figure_img_id.append(i['img']['@id'])
+    figure_img_he.append(i['img']['@he'])
+    figure_img_wi.append(i['img']['@wi'])
+    figure_img_file.append(i['img']['@file'])
+    figure_img_alt.append(i['img']['@alt'])
+    figure_img_content.append(i['img']['@img-content'])
+    figure_img_format.append(i['img']['@img-format'])
+# print(figure_id)
+# print(figure_num)
+# print(figure_img_alt)
+# print(figure_img_content)
+# print(figure_img_file)
+# print(figure_img_format)
+# print(figure_img_id)
+# print(figure_img_wi)
+# print(figure_img_he)
 #description
 p =[]
 for i in range(len(doc['us-patent-grant']['description']['description-of-drawings']['p'])):
      p.append((doc['us-patent-grant']['description']['description-of-drawings']['p'])[i])
 # print(p[1])
 
+p_id = []
+p_num =[]
+p_text=[]
 
+for i in p:
+    p_id.append(i['@id'])
+    p_num.append(i['@num'])
+    p_text.append(i['#text'])
+# print(p_id)
